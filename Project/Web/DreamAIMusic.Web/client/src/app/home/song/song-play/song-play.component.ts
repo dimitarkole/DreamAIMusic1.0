2a87dfa8149aab222beadfd599bc1a9f2ea30bf5
Dimitar Kolev <37376335+dimitarkole@users.noreply.github.com>
2020-08-08 00:09:13 +0300

    add play song page

---
+++ Project/Web/DreamAIMusic.Web/client/src/app/home/song/song-play/song-play.component.ts
@@ -0,0 +1,121 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import Category from '../../../components/shared/models/category';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Song } from '../../../components/shared/models/song';
+import { CategoryService } from '../../../core/services/category.service';
+import { SongService } from '../../../core/services/song.service';
+import { HttpEventType } from '@angular/common/http';
+
+@Component({
+  selector: 'app-song-play',
+  templateUrl: './song-play.component.html',
+  styleUrls: ['./song-play.component.css']
+})
+export class SongPlayComponent implements OnInit {
+  songId: string;
+  categories$: Observable<Category[]>
+  nameMinLength = 2;
+  nameMaxLength = 30;
+  textMinLength = 10;
+  textMaxLength = 1000;
+  songForm: FormGroup
+  public progress: number;
+  public message: string;
+  private routeUpload: string = 'song/Upload';
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private songService: SongService) {
+    this.categories$ = categoryService.all();
+  }
+
+  ngOnInit() {
+    let song: Song = this.route.snapshot.data.song;
+    console.log(song);
+    this.songId = song.id;
+    this.songForm = this.formBuilder.group({
+      id: song.id,
+      name: [
+        song.name,
+        [
+          Validators.required,
+          Validators.minLength(this.nameMinLength),
+          Validators.maxLength(this.nameMaxLength)
+        ]
+      ],
+      imageFile: [
+        song.imageFile,
+        [
+          // Validators.required
+        ]
+      ],
+      path: [
+        song.path,
+        [
+          // Validators.required
+        ]
+      ],
+      musicCategoryId: [
+        song.musicCategoryId,
+        [
+          Validators.required,
+          Validators.pattern('^((?!default).)*$'),
+        ]
+      ],
+      text: [
+        song.text,
+        [
+          Validators.required,
+          Validators.minLength(this.textMinLength),
+          Validators.maxLength(this.textMaxLength)
+        ]
+      ]
+    })
+  }
+
+  formHandler() {
+    let song: Song = this.songForm.value;
+    this.songService.edit(song, this.songId)
+      .subscribe(_ => {
+        this.router.navigate(['song', 'all']);
+      })
+
+    this.songForm.reset();
+  }
+
+  get name(): AbstractControl {
+    return this.songForm.get('name');
+  }
+
+  get path(): AbstractControl {
+    return this.songForm.get('path');
+  }
+
+  get text(): AbstractControl {
+    return this.songForm.get('text');
+  }
+
+  get musicCategoryId(): AbstractControl {
+    return this.songForm.get('musicCategoryId');
+  }
+
+  get imageFile(): AbstractControl {
+    return this.songForm.get('imageFile');
+  }
+
+  public uploadImage = (files) => {
+    this.songService.uploadImage(files).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress)
+        this.progress = Math.round(100 * event.loaded / event.total);
+      else if (event.type === HttpEventType.Response) {
+        this.message = 'Upload success.';
+        //this.onUploadFinished.emit(event.body);
+        // this.songForm.controls['imageFile'].setValue(event.ok);
+      }
+    });
+  }
+}
