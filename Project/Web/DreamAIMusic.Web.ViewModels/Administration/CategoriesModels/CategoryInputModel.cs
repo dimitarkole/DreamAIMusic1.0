1239783ecc39ab04d1ced956f10e2afdbaf96381
Венета Тончева <veneta.toncheva@edu.mon.bg>
2020-09-21 09:10:20 +0300

    create unit test for CategoryService All pass successfull

--- Project/Web/DreamAIMusic.Web.ViewModels/Administration/CategoriesModels/CategoryInputModel.cs
+++ Project/Web/DreamAIMusic.Web.ViewModels/Administration/CategoriesModels/CategoryInputModel.cs
@@ -1,10 +1,12 @@
-﻿using DreamAIMusic.Services.Mapping;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using DreamAIMusic.Data.Models;
-namespace DreamAIMusic.Web.ViewModels.Administration.CategoriesModels
+﻿namespace DreamAIMusic.Web.ViewModels.Administration.CategoriesModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using DreamAIMusic.Data.Models;
+    using DreamAIMusic.Services.Mapping;
+
     public class CategoryInputModel : IMapTo<Category>
     {
         public string Name { get; set; }
